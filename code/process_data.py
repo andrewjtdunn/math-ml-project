bf589983a67f98f0cbb2960cfdcd12821f3a9d56
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2023-11-21 23:07:59 -0600

    debug process_data

--- code/process_data.py
+++ code/process_data.py
@@ -1,12 +1,17 @@
 import numpy as np
+from sklearn import preprocessing
 
 
-def normalize_X_matrix(X):
+def standardize_matrix(X):
     """ """
     num_cols = X.shape[1]
-    norm_matrix = np.zeroes(X.shape)
+    norm_matrix = np.zeros(X.shape)
 
-    for col in range(num_cols):
-        norm_matrix[:, col] = np.linalg.norm(X[:, col])
+    for col_num in range(num_cols):
+        X_col = X[:, col_num]
+        norm_matrix[:, col_num] = (X_col - np.mean(X_col)) / np.std(X_col)
+
+    # scaler = preprocessing.StandardScaler().fit(X)
+    # X_scaled = scaler.transform(X)
 
     return norm_matrix
