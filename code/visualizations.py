8af0b3a27e4d2a9e202754dd523f7d1489d732cb
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2023-12-08 12:30:21 -0600

    add graph of label frequency, and tweak accuracy metrics

--- code/visualizations.py
+++ code/visualizations.py
@@ -11,6 +11,27 @@ def scatter_graph(df):
     plt.show()
 
 
+def make_label_distribution_graph(y):
+    """ """
+    label_numbers_and_test = [
+        "0 - Euthanasia",
+        "1 - Adoption",
+        " 2 - Foster",
+        "3 - Return",
+        "4 - Transfer",
+        "5 - No Outcome",
+    ]
+    plt.bar(
+        label_numbers_and_test,
+        np.bincount(y, minlength=6),
+        color=["red", "green", "purple", "orange", "blue", "yellow"],
+    )
+    plt.xticks(rotation=45)
+    # plt.show()
+
+    return plt
+
+
 def make_confusion_matrix(y, y_hat):
     """ """
     unique_labels = np.unique(np.concatenate([y, y_hat]))
@@ -35,13 +56,14 @@ def make_hyperparameter_graph(metrics, scores, xlabel):
     # plot the stats
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
 
-    plt.plot(metrics, accuracy_list, label="accuracy", color="blue")
-    plt.plot(metrics, precision_list, label="precision", color="red")
-    plt.plot(metrics, recall_list, label="recall", color="darkgreen")
-    plt.plot(metrics, fl_list, label="f1", color="thistle")
+    # plt.plot(metrics, accuracy_list, label="Accuracy (weighted)", color="blue")
+    plt.plot(metrics, precision_list, label="Precision (weighted)", color="red")
+    plt.plot(metrics, recall_list, label="Recall (weighted)", color="darkgreen")
+    plt.plot(metrics, fl_list, label="F1 (weighted)", color="thistle")
 
     ax.legend(loc="lower right", fontsize=16)
     ax.set_xlabel(xlabel, fontsize=16)
     ax.set_xscale("log")
     ax.set_ylabel("Performance stats", fontsize=16)
-    plt.show()
+
+    return plt
